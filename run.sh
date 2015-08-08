8d7d91c7afa1f688242aa812d7647b21560a38df
Honglai <502089948@163.com>
2015-08-08 23:30:36 +1000

    forst commit

---
+++ run.sh
@@ -0,0 +1,2 @@
+vagrant reload
+vagrant ssh -c 'cd rails-in-action/simple_app && rails s -b 0.0.0.0'
---
+++ run_sinatra.sh
@@ -0,0 +1,2 @@
+vagrant reload
+vagrant ssh -c 'cd sinatra-in-action && ruby myapp.rb -o 0.0.0.0'
